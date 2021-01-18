9a17fb954c632a5e8114626feda85e1f00bdfaae
Иван <johnny.kiv@yandex.ru>
2021-01-18 20:21:22 +0300

    Update proge.pas

--- proge.pas
+++ proge.pas
@@ -1,40 +1,36 @@
 ﻿program Шахматы;
 uses graphABC;
-var width:integer;
-shag:real;
+var 
+  width:integer;
+  shag:real;
+  x:real;
+procedure zakrash(x:real;y:integer;col:integer);
+begin
+    
+    x:=shag/2;
+    if col=1 then
+      x:=x+shag;
+    if col=2 then
+      x:=x+shag/2;
+
+end;
 begin
   SetWindowTitle('Шахматы');
   readln(width);
   SetWindowSize(width,width);
   shag:=width/8;
-  for var i:=1 to 8 do 
+  for var i:=0 to 8 do 
   begin
     line(0,Round(shag*i),width,Round(shag*i));
   end;
   shag:=width/8;
-  for var i:=1 to 8 do 
+  for var i:=0 to 8 do 
   begin
     line(Round(shag*i),0,Round(shag*i),width);
   end;
- {for  var i:=1 to 4 do 
-    begin
-      FloodFill(y[i],25,clWhite);
-      FloodFill(y[i],25+50,clBlack);
-      FloodFill(y[i],25+50*2,clWhite);
-      FloodFill(y[i],25+50*3,clBlack);
-      FloodFill(y[i],25+50*4,clWhite);
-      FloodFill(y[i],25+50*5,clBlack);
-      FloodFill(y[i],25+50*6,clWhite);
-      FloodFill(y[i],25+50*7,clBlack);
-      
-      
-      FloodFill(y[i]+50,25,clBlack);
-      FloodFill(y[i]+50,25+50,clWhite);
-      FloodFill(y[i]+50,25+50*2,clBlack);
-      FloodFill(y[i]+50,25+50*3,clWhite);
-      FloodFill(y[i]+50,25+50*4,clBlack);
-      FloodFill(y[i]+50,25+50*5,clWhite);
-      FloodFill(y[i]+50,25+50*6,clBlack);
-      FloodFill(y[i]+50,25+50*7,clWhite);
-    end;}
+  for var i:=0 to 7 do
+  begin
+      zakrash(x,Rotade(shag/2),1);
+  end;
+   
 end.
