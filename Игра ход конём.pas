8cafd423a14ef195c099ef8139a499984b4ebb00
Иван <johnny.kiv@yandex.ru>
2021-01-24 12:40:40 +0300

    Update Игра ход конём.pas

--- Игра ход конём.pas
+++ Игра ход конём.pas
@@ -1,10 +1,26 @@
 ﻿{Эта игра "Ход Конём"}
 {Автор: johnny kiv}
-program Шахматы;
-uses graphABC;
+
+program ИграХодКонём;
+ 
+uses
+  GraphABC;
 var 
 width:integer;
 shag:real;
+procedure MouseDown(x, y, m: integer);
+begin
+  m:=0;    
+  OnMouseDown := (x,y,mb) -> FloodFill(x,y,clBlue);
+  var b:=2;
+  while  b<0 do
+  begin
+    b:=b-1;
+    m:=m+1;
+    if m=2 then
+      FloodFill(30,30,clRed);
+  end;
+end;
 begin
   SetWindowTitle('Шахматы');
   width:=600;
@@ -18,6 +34,6 @@ begin
   begin
     line(Round(shag*i),0,Round(shag*i),width);
   end;
-  OnMouseDown := (x,y,mb) -> FloodFill(x,y,clBlue);
   
+  OnMouseDown:=MouseDown;
 end.
